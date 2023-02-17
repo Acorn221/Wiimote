432e46af6162130756e45c38548ed5168df5c0ee
James <jamesarnott2@gmail.com>
2023-02-17 15:33:55 +0000

    fix: search for manufacturer in ScanDevices fixed

--- include/ScanDevices.cpp
+++ include/ScanDevices.cpp
@@ -3,6 +3,7 @@
 // ScanDevices constructor
 ScanDevices::ScanDevices() {
 	scanned_device_list_size = 0;
+	searchForManufacturer = false;
 }
 
 /**
@@ -11,7 +12,7 @@ ScanDevices::ScanDevices() {
  * @param bd_addr 
  * @return int The index of the device in the scanned device list
  */
-static int ScanDevices::scanned_device_find(struct bd_addr_t* bd_addr) {
+int ScanDevices::scanned_device_find(struct bd_addr_t* bd_addr) {
   for (int i = 0; i < scanned_device_list_size; i++) {
     scanned_device_t* c = &scanned_device_list[i];
     if (memcmp(&bd_addr->addr, c->bd_addr.addr, BD_ADDR_LEN) == 0) {
@@ -27,23 +28,23 @@ static int ScanDevices::scanned_device_find(struct bd_addr_t* bd_addr) {
  * @param scanned_device 
  * @return int 
  */
-static int ScanDevices::scanned_device_add(struct scanned_device_t scanned_device) {
+int ScanDevices::scanned_device_add(struct scanned_device_t scanned_device) {
   if (SCANNED_DEVICE_LIST_SIZE == scanned_device_list_size) {
     return -1;
   }
 
-	if(manu == NULL) {
+	if(searchForManufacturer) {
 		log_d("No target manufacturer set, adding device to list");
 		scanned_device_list[scanned_device_list_size++] = scanned_device;
 
 	} else {
 		bool foundManu = false;
 		// creating a temp variable to iterate through the linked list
-		manufacturer_t curr = *manu;
+		manufacturer_t curr = target_manufacturers;
 		
 		// Check the manufacturer of the device
 		while(curr.next != NULL) {
-			if(memcmp(&scanned_device.bd_addr.addr, manu.manufacturer, 3) == 0) {
+			if(memcmp(&scanned_device.bd_addr.addr, curr.manufacturer, 3) == 0) {
 				log_d("Found device from target manufacturer, adding device to list");
 				scanned_device_list[scanned_device_list_size++] = scanned_device;
 				foundManu = true;
@@ -56,17 +57,23 @@ static int ScanDevices::scanned_device_add(struct scanned_device_t scanned_devic
   return scanned_device_list_size;
 }
 
-static void ScanDevices::scanned_device_clear(void) {
+void ScanDevices::scanned_device_clear(void) {
   scanned_device_list_size = 0;
 	log_d("Scanned device list cleared");
 }
 
-static void ScanDevices::set_target_manufacturer(struct manufacturer_t manu) {
-	target_manufacturer = manu;
-	logd("Target manufacturer set");
+void ScanDevices::clear_target_manufacturer(void) {
+	searchForManufacturer = false;
+	log_d("Target manufacturer cleared");
+}
+
+void ScanDevices::set_target_manufacturer(struct manufacturer_t manu) {
+	target_manufacturers = manu;
+	searchForManufacturer = true;
+	log_d("Target manufacturer set");
 	while(manu.next != NULL) {
 		manu = *manu.next;
-		logd("Target manufacturer set: %02X:%02X:%02X", manu.manufacturer[0], manu.manufacturer[1], manu.manufacturer[2]);
+		log_d("Target manufacturer set: %02X:%02X:%02X", manu.manufacturer[0], manu.manufacturer[1], manu.manufacturer[2]);
 	}
 }
 /**
@@ -76,13 +83,13 @@ static void ScanDevices::set_target_manufacturer(struct manufacturer_t manu) {
  * @return true 
  * @return false 
  */
-static bool ScanDevices::scan_for_devices (bool scan) {
+bool ScanDevices::scan_for_devices (bool scan) {
 		if(scan){
 			// Clear the scanned device list
 			scanned_device_clear();
 			// Start scanning
 			uint8_t timeout = 10; //0x30;
-			uint16_t len = make_cmd_inquiry(tmp_data, 0x9E8B33, timeout, 0x00);
+			uint16_t len = make_cmd_inquiry(*tmp_data, 0x9E8B33, timeout, 0x00);
 			if(_queue_data(_tx_queue, tmp_data, len) != ESP_OK){
 				log_d("Error sending inquiry command on ScanDevices");
 				return false;
